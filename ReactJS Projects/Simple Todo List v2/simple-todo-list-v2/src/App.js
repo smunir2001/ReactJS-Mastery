8bc0d78b01628a30b3b2e558fdc6c5c44e7416ee
Sami Munir <smunir2001@gmail.com>
2023-03-04 12:32:14 -0500

    Header component complete

--- ReactJS Projects/Simple Todo List v2/simple-todo-list-v2/src/App.js
+++ ReactJS Projects/Simple Todo List v2/simple-todo-list-v2/src/App.js
@@ -1,11 +1,11 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header/component.Header';
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
-      <h2>Sami Munir</h2>
-      <h3>Simple Todo List-v2</h3>
+      <Header />
     </>
   );
 }
---
+++ ReactJS Projects/Simple Todo List v2/simple-todo-list-v2/src/components/Header/component.Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.styles.css';
+
+export default function Header() {
+    return (
+        <div className='Header'>
+            <h1>Simple Todo List-v2</h1>
+            <h2>Sami Munir - Rutgers University</h2>
+            <h3>ReactJS Mastery</h3>
+        </div>
+    );
+}
