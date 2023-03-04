50ff2710e8c3f4164308ff0e672a8807b0670198
Sami Munir <smunir2001@gmail.com>
2023-03-04 11:55:53 -0500

    file cleanup & restart components

--- ReactJS Projects/Simple Todo List v2/simple-todo-list-v2/src/App.js
+++ ReactJS Projects/Simple Todo List v2/simple-todo-list-v2/src/App.js
@@ -1,15 +1,8 @@
 import './App.css';
-import Header from './components/Header/Header';
-import TodoList from './components/TodoList/TodoList';
-import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <>
-      <Header />
-      <TodoList />
-      <Footer />
-    </>
+    <h1>Hello World!!!!!!!</h1>
   );
 }
 
