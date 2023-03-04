74fbe604e39fc6f0b2aff8fa217c09ce91e30c85
Sami Munir <smunir2001@gmail.com>
2023-03-04 12:05:54 -0500

    project fresh start

--- ReactJS Projects/Simple Todo List v2/simple-todo-list-v2/src/App.js
+++ ReactJS Projects/Simple Todo List v2/simple-todo-list-v2/src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Hello World!!!!!!!</h1>
+    <>
+      <h1>Hello World</h1>
+      <h2>Sami Munir</h2>
+      <h3>Simple Todo List-v2</h3>
+    </>
   );
 }
 
