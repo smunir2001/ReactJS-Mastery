86913bb7726a343579a8993872735355631ad011
Sami Munir <smunir2001@gmail.com>
2023-03-04 12:56:41 -0500

    added TodoList component

--- ReactJS Projects/Simple Todo List v2/simple-todo-list-v2/src/App.js
+++ ReactJS Projects/Simple Todo List v2/simple-todo-list-v2/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/component.Header';
+import TodoList from './components/TodoList/component.TodoList';
 
 function App() {
   return (
     <>
       <Header />
+      <TodoList />
     </>
   );
 }
---
+++ ReactJS Projects/Simple Todo List v2/simple-todo-list-v2/src/components/TodoList/component.TodoList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './TodoList.styles.css';
+
+export default function TodoList() {
+    return (
+        <div className='TodoList'>
+            <div className='input'>
+                <h1>Input Div</h1>
+            </div>
+            <div className='output'>
+                <h1>Output Div</h1>
+            </div>
+        </div>
+    );
+}
